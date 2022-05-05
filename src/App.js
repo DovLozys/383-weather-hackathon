c68e4a1007361ffb305ae5e8e17681530b9553d7
troy <troywebb95@gmail.com>
2022-05-05 13:16:35 +0100

    useEffect fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useFetch from "react-fetch-hook";
 
-import Card from './components/Card/Card'
+import Card from "./components/Card/Card";
 
 import css from "./App.module.css";
 
@@ -9,8 +9,10 @@ function App() {
   const [searchInput, setSearchInput] = useState("");
   const [searchTerm, setSearchTerm] = useState("london");
   const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
   const [latitude, setLatitude] = useState(51.5085);
   const [longitude, setLongitude] = useState(-0.1257);
+  const [weeklyForecast, setWeeklyForecast] = useState([]);
 
   const handleChange = (e) => {
     setSearchInput(e.target.value);
@@ -20,15 +22,35 @@ function App() {
     setSearchTerm(searchInput);
   };
 
-  const dailyForecast = useFetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
-  );
+  // const dailyForecast = useFetch(
+  //   `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+  // );
 
-  const weeklyForecast = useFetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
-  );
+  // const weeklyForecast = useFetch(
+  //   `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+  // );
 
-  console.log("dailyForecast", dailyForecast);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+      );
+      const data = await response.json();
+      setData(data);
+      setIsLoading(false);
+      console.log(data);
+    }
+    fetchData();
+  }, [searchTerm]);
+
+  const fetchWeeklyForecast = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
   console.log("weeklyForecast", weeklyForecast);
 
   if (weeklyForecast.isLoading) {
@@ -39,11 +61,10 @@ function App() {
     <div className={css.App}>
       <h1>383 Weather App</h1>
       <main className="container">
-        {!weeklyForecast.isLoading && 
+        {!weeklyForecast.isLoading &&
           weeklyForecast.data.daily.map((day) => {
-            return <Card dailyForecast={day} />
-          })
-        }
+            return <Card dailyForecast={day} />;
+          })}
       </main>
     </div>
   );
