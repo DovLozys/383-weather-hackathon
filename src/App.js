0c34ab96aae34109943d5467bb67f9e10a0d2480
troy <troywebb95@gmail.com>
2022-05-05 14:15:56 +0100

    added media query for mapped cards for desktop display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import useFetch from "react-fetch-hook";
 
-import Card from './components/Card/Card'
+import Card from "./components/Card/Card";
 
 import css from "./App.module.css";
 
@@ -38,12 +38,11 @@ function App() {
   return (
     <div className={css.App}>
       <h1>383 Weather App</h1>
-      <main className="container">
-        {!weeklyForecast.isLoading && 
+      <main className={css.container}>
+        {!weeklyForecast.isLoading &&
           weeklyForecast.data.daily.map((day) => {
-            return <Card dailyForecast={day} />
-          })
-        }
+            return <Card dailyForecast={day} />;
+          })}
       </main>
     </div>
   );
